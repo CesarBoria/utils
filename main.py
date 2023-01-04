353a12b94abf8624034e0778335ac4935592def2
Joaquin Hernandez Martinez <noethercode@gmail.com>
2023-01-04 11:42:35 +0100

    Añadida funcionalidad para el input de usuario

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 # Main Python file
 from interface import Interfaz
 
+def convertir(input) -> int:
+    try:
+        output = int(input)
+    except ValueError:
+        try:
+            input = float(input)
+        except ValueError:
+            print('No es un numero')
+            return None # TODO cambiarlo para que sea mas elegante
+        else:
+            output = str(input).split('.')
+            output = int(output[0])
+
+    return output
+
 if __name__ == "__main__":
     _interface = Interfaz()
     _interface.show()
-    user_input = input(">>> ")
+    user_input = convertir(input(">>> "))

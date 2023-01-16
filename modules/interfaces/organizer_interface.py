d770cb5c11e0f4917f262af02ffe51fa1a9e1acf
Joaquin Hernandez Martinez <noethercode@gmail.com>
2023-01-16 11:35:23 +0100

    Empezado el organizer, interfaz mas o menos clara

---
+++ modules/interfaces/organizer_interface.py
@@ -0,0 +1,20 @@
+MOD_INTERFACE = {
+'HEAD': """
+ORGANIZER
+-----------
+
+0 PATH - Change, set or show the working path (where the tool can make changes)
+1 DO <COMMAND> - Copy (CP), move (MV) or remove (RM) files and directories
+2 ORDER - Orders the files inside the directory according to extension
+3 SHOW - Shows all the information about the current (working) directory
+4 HIDDEN - Shows and creates hidden files and directories
+5 LINK - Creates symlinks
+
+"""
+}
+
+class Explorer:
+    def __init__(self):
+        pass
+    def __repr__(self):
+        pass
--- modules/organizer.py
+++ modules/organizer.py
@@ -1,7 +1,9 @@
 from modules.interfaces.general_interface import BASE, selector
+from modules.interfaces.organizer_interface import MOD_INTERFACE, Explorer
 
 
 def mod_main():
-
+    print(MOD_INTERFACE['HEAD'])
+    
     out = input(BASE)
     selector(out)

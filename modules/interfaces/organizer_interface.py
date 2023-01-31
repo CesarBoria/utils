68e787f0c5abab82657ef87e7296c58e78b37296
Joaquin Hernandez Martinez <noethercode@gmail.com>
2023-01-31 21:15:18 +0100

    Terminado DO (no testeado)

--- modules/interfaces/organizer_interface.py
+++ modules/interfaces/organizer_interface.py
@@ -3,6 +3,8 @@ MOD_INTERFACE = {
 ORGANIZER
 ---------
 
+Docs are available with <COMMAND> INFO
+
 0 PATH - Change, set or show the working path (where the tool can make changes)
 1 DO <COMMAND> - Copy (CP), move (MV) or remove (RM) files and directories
 2 ORDER - Orders the files inside the directory according to extension
@@ -21,5 +23,50 @@ Example: Moving the file info.txt from /home to /home/dir
 
 """
 ,
-# Luego se añadiran opciones de info
+'PATH':
+"""
+PATH - Shows the current directory
+PATH <VALID_DIR> - Changes the current directory to the specified one
+
+<VALID_DIR> follows the usual path rules. Relative paths can be done with
+~ (home), .. (parent) and / (Linux root)
+
+Info about the current path can be listed with the SHOW command
+
+The current directory is the standart path for the other commands of the tool
+
+"""
+,
+'DO':
+"""
+CP <FILE_OR_DIR> - Copies the file or directory to the current working directory
+CP <FILE_OR_DIR> <DESTINY_DR> - Copies the file or directory to a specified directory
+MV <FILE_OR_DIR> - Moves the file or directory to the current working directory
+MV <FILE_OR_DIR> <DESTINY_DIR> - Moves the file or directory to a specified directory
+RM <FILE_OR_DIR> - Deletes the file or directory
+
+<FILE_OR_DIR> and <DESTINY_DIR> will take PATH current directory
+as the relative path, unless an absolute path is specified
+
+"""
+,
+'ORDER':
+"""
+
+"""
+,
+'SHOW':
+"""
+
+"""
+,
+'HIDDEN':
+"""
+
+"""
+,
+'LINK':
+"""
+
+"""
 }
--- modules/organizer.py
+++ modules/organizer.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import shutil
 from main import crear_interfaz
 from modules.interfaces.general_interface import BASE, selector
 from modules.interfaces.organizer_interface import MOD_INTERFACE
@@ -25,13 +26,9 @@ def mod_main():
         choice = input("The command doesn't exist or is not recognized. Exit?\n >>> ")
         if choice.lower() in ('y', 'yes', 'si', 'back', 'return', 'go back'):
             crear_interfaz()
-        else:
-            mod_main()
     except Exception as e:
         print('There was an error, please, copy and paste this following text and send it to my github, thanks :)')
         print(repr(e))
-        out = input(BASE)
-        selector(out)
     else: # TODO: Refactorizar a match/case en Python 3.11
         if str(_input[0]).lower() == 'path': # TODO: Crear docs para PATH
             if len(_input) == 1:
@@ -42,7 +39,19 @@ def mod_main():
                 what_is_this = _input[3:]
                 print(f'{what_is_this} not recognized, only {_input[1]} will be used')
         elif str(_input[0]).lower() == 'do':
-            pass
+            try:
+                if _input[1].lower() in ('cp', 'copy', '-c'):
+                    do_command('copy', _input[2:])
+                elif _input[1].lower() in ('mv', 'move', '-m'):
+                    do_command('move', _input[2:])
+                elif _input[1].lower() in ('cp', 'remove', 'delete', 'del', '-d'):
+                    do_command('remove', _input[2:])
+                else:
+                    raise ValueError
+            except Exception as e:
+                print(repr(e))
+                print("Incorrect syntax, restarting the tool")
+                mod_main()
         elif str(_input[0]).lower() == 'order':
             pass
         elif str(_input[0]).lower() == 'show':
@@ -52,26 +61,58 @@ def mod_main():
         elif str(_input[0]).lower() == 'link':
             pass
         else:
-            print('Command not recognized, please try again') # TODO: Es necesario?
+            print('Command not recognized. Introduce START to restart the tool') # TODO: Es necesario?
+            out = input(BASE)
+            if out.lower() in ('start', 'restart', 're', 'again'):
+                mod_main()
+            selector(out)
+            first_print = MOD_INTERFACE['HEAD']
         mod_main()
 
 
 def get_path(_input: str) -> str:
-    if _input[0] in ('..', os.sep):
-        os.chdir(_input)
-        print(f'New path set to {os.getcwd()}')
-    elif _input[0] == '~':
-        os.chdir(os.environ['HOME'])
-        print(f'New path set to HOME ({os.getcwd()})')
-    else:
-        new_path = os.path.join(os.getcwd(), _input)
-        os.chdir(new_path)
-        print(f'New path set to {new_path}')
-    return os.getcwd()
+    try:
+        if _input[0] in ('.', os.sep):
+            os.chdir(_input)
+            print(f'New path set to {os.getcwd()}')
+        elif _input[0] == '~':
+            new_path = _input.replace('~', os.environ['HOME'])
+            os.chdir(new_path)
+            print(f'New path set to {os.getcwd()}')
+        else:
+            new_path = os.path.join(os.getcwd(), _input)
+            os.chdir(new_path)
+            print(f'New path set to {new_path}')
+        return os.getcwd()
+    except FileNotFoundError:
+        print('There is no directory with that name')
+        mod_main()
 
 
-def do_command(_input: str = '') -> str:
-    pass
+def do_command(command: str, _input: list[str]):
+    start = _input[0]
+    if command == 'remove':
+        if input(f'WARNING: You are about to remove {start}, are you sure?') in ('yes', 'y', 'si'):
+            if os.path.isfile(start):
+                os.remove(start)
+            else:
+                shutil.rmtree(start)
+            print(f'{start} has been removed')
+        return None
+    try:
+        end = _input[1]
+    except IndexError:
+        end = os.getcwd()
+    if command == 'copy':
+        if os.path.isfile(start):
+            new_path = shutil.copy2(start, end)
+            print(f'{start} file copied to {new_path}')
+        else:
+            new_path = shutil.copytree(start, end)
+            print(f'{start} directory copied to {new_path}')
+    if command == 'move':
+        new_path = shutil.move(start, end)
+        print(f'{start} moved to {new_path}')
 
 
 def order_files(_input: str = '') -> str:
@@ -91,9 +132,16 @@ def make_links(_input: str = '') -> str:
 
 
 def parse_tool() -> list[str]:
+    global first_print
     _input = input(" >>> ").split()
+    if _input[0].lower() in ('exit', 'back', 'return', 'go back', 'menu', 'start', 'restart'):
+        first_print = MOD_INTERFACE['HEAD']
+        crear_interfaz()
     if _input[0] in [str(option) for option in tuple(dict_with_commands.keys())]:
         _input[0] = dict_with_commands[int(_input[0])]
+    if _input[1].lower():
+        print(MOD_INTERFACE[_input[0].upper()])
+        mod_main()
     return _input
 
 
